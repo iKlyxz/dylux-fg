26c9beb0cee0038ad5b3e4b29001f20d9395e2b1
iKlyxz <107784211+iKlyxz@users.noreply.github.com>
2022-06-27 19:54:57 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -15,10 +15,10 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.sendButton(m.chat, `*≡ Enlace Detectado*
+        await conn.sendButton(m.chat, `*≡ 𝐄𝐍𝐋𝐀𝐂𝐄 𝐃𝐄𝐓𝐄𝐂𝐓𝐀𝐃𝐎*
             
-No permitimos enlaces de otros grupos 
-lo siento *${await this.getName(m.sender)}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)
+𝐂𝐇𝐎𝐋𝐎 𝐈𝐃𝐈𝐎𝐓𝐀 𝐐𝐔𝐄 𝐂𝐇𝐂𝐇 𝐄𝐍𝐕𝐈𝐀𝐒
+𝐋𝐈𝐍𝐊 𝐃𝐄 𝐓𝐔 𝐆𝐑𝐔𝐏𝐎 𝐃𝐄 𝐄𝐒𝐏𝐄𝐂𝐈𝐀𝐋𝐄𝐒 *${await this.getName(m.sender)}*  𝐕𝐄 𝐀 𝐉𝐎𝐃𝐄𝐑 𝐀 𝐎𝐓𝐑𝐎 𝐋𝐀𝐃𝐎 𝐑𝐄𝐂𝐎𝐍𝐂𝐇𝐀𝐃𝐄𝐓𝐔𝐌𝐀𝐃𝐑𝐄😾 ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)
         if (isBotAdmin && chat.antiLink) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
         } else if (!chat.antiLink) return //m.reply('')
