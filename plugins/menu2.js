37faf2763d0a277fb37cb6f6cb842b17538864f7
iKlyxz <107784211+iKlyxz@users.noreply.github.com>
2022-06-27 20:10:21 -0500

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -21,7 +21,7 @@ envíame por t.me/fgsupp_bot el *audio + comando* con en el que responderá
 //const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     let pp = './src/fg_logo.jpg' 
     
-    conn.sendHydrated(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, 'https://youtube.com/fg98f', 'YouTube', null, null, [
+    conn.sendHydrated(m.chat, m2, '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/not.sxrgio\n', pp, 'https://facebook.com/iTzMekingWT', 'Facebook', null, null, [
      ['⏍ Info', '/botinfo'],
       ['⌬ Grupos', '/gpdylux']
     ], m)
